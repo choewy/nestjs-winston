005b02e4613e719c3bc916107fb45f6d359b416b
choewy <choewy32@gmail.com>
2024-03-20 16:02:08 +0900

    feature/interface comments

--- libs/interfaces/winston-logger-factory-options.interface.ts
+++ libs/interfaces/winston-logger-factory-options.interface.ts
@@ -1,6 +1,16 @@
 import { WinstonCreateFileName } from './winston-create-filename.interface';
 import { WinstonLoggerLevel } from './winston-logger-level.interface';
 
+/**
+ * Winston Logger Factory Cconstructor Options
+ *
+ * @param name logger context name
+ * @param dirname log directory name
+ * @param dataPattern log file date format
+ * @param createFilename create log filename function
+ * @param maxSize max size of log file
+ * @param maxFiles max days of log file
+ */
 export interface WinstonLoggerFactoryOptions {
   name?: string;
   dirname?: string;
@@ -10,6 +20,12 @@ export interface WinstonLoggerFactoryOptions {
   maxFiles?: string;
 }
 
+/**
+ * Create Logger Service(Winston Logger) Options
+ *
+ * @param consoleLevel logger levels for console
+ * @param fileLevel logger levels for file
+ *  */
 export interface WinstonLoggerFactoryCreateOptions {
   consoleLevel?: WinstonLoggerLevel[];
   fileLevel?: WinstonLoggerLevel[];
